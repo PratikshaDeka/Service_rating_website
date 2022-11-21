61be31e47712622549801de659eedeabc6b9df48
Pratiksha Deka <pratikshadeka@Pratikshas-MacBook-Air.local>
2022-11-21 23:45:11 +0530

    adding all the files

---
+++ SRW/backend/src/articles/models.py
@@ -0,0 +1,11 @@
+
+
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+
+    def _str_(self):
+        return self.title
